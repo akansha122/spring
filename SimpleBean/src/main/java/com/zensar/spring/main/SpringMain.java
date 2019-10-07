f01c23591dcf0239dc9ec6c702c4f7baad0ee878
Akansha Shah <shahakansha23@gmail.com>
2019-10-07 15:07:10 -0700

    added xml scope

--- SimpleBean/src/main/java/com/zensar/spring/main/SpringMain.java
+++ SimpleBean/src/main/java/com/zensar/spring/main/SpringMain.java
@@ -8,13 +8,13 @@ import com.zensar.spring.Greeting;
 public class SpringMain {
 	public static void main(String[] args) {
 		//creating the object of sping IOC container
-		ApplicationContext ctx= new ClassPathXmlApplicationContext("greetings.xml");	
+		ApplicationContext ctx= new ClassPathXmlApplicationContext("greetings.xml");
+		
 		//retriving object of anniversaryGreeting.
 		Greeting greeting1=ctx.getBean("festival", Greeting.class);
-		System.out.println(greeting1.greet());
+		//System.out.println(greeting1.greet());
 		
-		Greeting greeting2=ctx.getBean("anniversary", Greeting.class);
-		System.out.println(greeting2.greet());
+		Greeting greeting2=ctx.getBean("festival", Greeting.class);
+		//System.out.println(greeting2.greet());
 	}
-
 }
